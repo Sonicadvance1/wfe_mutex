2cbe5eaf869f22991645ffd2d16ff86d8d6b95b0
Ryan Houdek <Sonicadvance1@gmail.com>
2023-12-30 19:51:23 -0800

    fix arm build

--- unittests/cpp_test.cpp
+++ unittests/cpp_test.cpp
@@ -6,11 +6,31 @@
 #include <stdio.h>
 #include <thread>
 
+#ifdef _M_ARM_64
+uint64_t read_cycle_counter() {
+	uint64_t result;
+	__asm volatile(
+		"isb;"
+		"mrs %[Res], CNTVCT_EL0;"
+		: [Res] "=r" (result));
+	return result;
+}
+#elif defined(_M_X86_64)
+uint64_t read_cycle_counter() {
+	return __rdtsc();
+}
+#else
+uint64_t read_cycle_counter() {
+	return std::chrono::high_resolution_clock::now().time_since_epoch().count();
+}
+#endif
+
+
 void Thread_wfe(uint8_t *Data, std::atomic<uint8_t> *Ready, std::atomic<uint8_t> *Done, std::atomic<uint64_t>* Time) {
 	while (Ready->load() == 0);
 
 	wfe_mutex_wait_for_value_i8(Data, 1, false);
-	*Time = __rdtsc();
+	*Time = read_cycle_counter();
 	*Done = 1;
 }
 
@@ -19,7 +39,7 @@ void Thread_spin(uint8_t *Data, std::atomic<uint8_t> *Ready, std::atomic<uint8_t
 	while (Ready->load() == 0);
 	while (DataAtomic->load() == 0);
 
-	*Time = __rdtsc();
+	*Time = read_cycle_counter();
 	*Done = 1;
 }
 
@@ -43,7 +63,7 @@ void DoTest_wfe() {
 	// Wait for the thread to say we're done
 	while (Done->load() == 0);
 
-	const auto DoneTime = __rdtsc();
+	const auto DoneTime = read_cycle_counter();
 
 	auto Diff = DoneTime - *Time;
 	fprintf(stderr, "cycle difference between threads: %ld\n", Diff);
@@ -73,7 +93,7 @@ void DoTest_spin() {
 	// Wait for the thread to say we're done
 	while (Done->load() == 0);
 
-	const auto DoneTime = __rdtsc();
+	const auto DoneTime = read_cycle_counter();
 
 	auto Diff = DoneTime - *Time;
 	fprintf(stderr, "cycle difference between threads: %ld\n", Diff);
