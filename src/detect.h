b45d437dd4d2a5bd90cfd78a7f8d26df1261e3b3
Ryan Houdek <Sonicadvance1@gmail.com>
2024-01-15 13:04:36 -0800

    Fixes implicit forward declaration

--- src/detect.h
+++ src/detect.h
@@ -7,6 +7,7 @@
 
 extern wfe_mutex_features Features;
 
+void wfe_mutex_detect_features();
 static inline uint64_t wfe_mutex_detect_calculate_cycles_for_nanoseconds(uint64_t nanoseconds) {
 	return nanoseconds * Features.cycles_per_nanosecond_multiplier / Features.cycles_per_nanosecond_divisor;
 }
